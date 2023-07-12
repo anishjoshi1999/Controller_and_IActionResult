1897fe6cef1959b7a2297d5b3df266d577e28b47
anishjoshi1999 <anishjoshi1999@gmail.com>
2023-07-12 09:41:45 +0545

    JSON-Result

---
+++ Controller_and_IActionResult/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Controller_and_IActionResult.Models;
+namespace Controller_and_IActionResult.Controllers
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        [Route("/person")]
+        public JsonResult Person()
+        {
+            // Create a new Person object and initialize its properties
+            Person person = new Person()
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Anish",
+                LastName = "Joshi",
+                Age = 24
+            };
+
+            // Return the person object as a JsonResult
+            return Json(person);
+            //return JsonResult(person);
+        }
+    }
+}
--- Controller_and_IActionResult/Program.cs
+++ Controller_and_IActionResult/Program.cs
@@ -1,6 +1,11 @@
+using Controller_and_IActionResult.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
